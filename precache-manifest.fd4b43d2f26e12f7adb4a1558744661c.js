b75a9290adab7d8e4c054542d1d36466ec8368ed
Alireza Shajari <mahooresorkh@gmail.com>
2020-05-28 11:22:34 +0430

    this is the first commitment.

---
+++ precache-manifest.fd4b43d2f26e12f7adb4a1558744661c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd8e42d9dacb385e9a6cc8e2964de6f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "d44b5812113fcaab5a7b",
+    "url": "./static/css/main.64f50d30.chunk.css"
+  },
+  {
+    "revision": "b37a4e302c5c10de2ec4",
+    "url": "./static/js/2.ee7d3ba2.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "./static/js/2.ee7d3ba2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d44b5812113fcaab5a7b",
+    "url": "./static/js/main.7afbe4a0.chunk.js"
+  },
+  {
+    "revision": "01193d1c5cd25211b877",
+    "url": "./static/js/runtime-main.ce4b53e3.js"
+  },
+  {
+    "revision": "0035599f19e67e3f8f72d9683b5e8f5b",
+    "url": "./static/media/arrow-left-right.0035599f.svg"
+  },
+  {
+    "revision": "836513b43d595707f813f3034e2b6ac2",
+    "url": "./static/media/envelope-open-fill.836513b4.svg"
+  },
+  {
+    "revision": "855f8ec14f1bf77b53d51a2e685e1f18",
+    "url": "./static/media/heart-fill.855f8ec1.svg"
+  },
+  {
+    "revision": "5539fb8530dca33b2ea43c69959a9ff5",
+    "url": "./static/media/pen.5539fb85.svg"
+  },
+  {
+    "revision": "e1e98fe7b0cf18638a18c44ff80934d1",
+    "url": "./static/media/trash-fill.e1e98fe7.svg"
+  },
+  {
+    "revision": "a7510c1544493eb866241c475d65b9a9",
+    "url": "./static/media/x.a7510c15.svg"
+  }
+]);
